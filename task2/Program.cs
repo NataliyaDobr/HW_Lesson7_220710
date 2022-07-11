19fc8d8f7f4d0000d7a1354bdd195c1e452b098d
NataliyaDobr <n.m.suvorova@mail.ru>
2022-07-11 19:11:35 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int rows, int columns, int min, int max)
+{
+    int[,] answer = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            answer[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return answer;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+void FindArrayElement(int[,] array, int findX, int findY)
+{
+    bool isFind = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (i == findX-1 && j == findY-1)
+            {
+                isFind = true;
+                Console.WriteLine(array[i, j]);
+            }
+    }
+    if (!isFind)
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
+}
+
+int rows = Prompt("Введите количество строк > ");
+int columns = Prompt("введите количество столбцов > ");
+int[,] myArray = GenerateArray(rows, columns, -10, 10);
+PrintArray(myArray);
+
+int search_i = Prompt("Введите номер строки искомого элемента > ");
+int search_j = Prompt("Введите номер столбца искомого элемента > ");
+
+FindArrayElement(myArray,search_i,search_j);
